d0c933268b7d4646ff8158f2a93ff298a2f0b733
Faraz897 <farazasif934@gmail.com>
2024-03-11 06:32:35 +0500

    In Process

---
+++ app.js
@@ -0,0 +1,50 @@
+// function abc (){
+//     document.getElementById('abc') .innerHTML = 'hello world'
+// }
+
+// var a = 'In pariatur, corrupti eum est error sint expedita.'  
+// function abc (){
+//     document.getElementById('anc').innerHTML = a
+// }
+
+
+// function abc (){
+//     document.getElementById('faraz').innerHTML = 'hello world'
+//     document.getElementById('anc').style.display = 'none'
+// }
+
+
+
+
+
+
+
+function print(e){
+    document.getElementById('inp').value += e
+}
+
+
+
+function eq(){
+    var a =  document.getElementById('inp')
+    a.value = eval(a.value)
+}
+
+
+function clrall(){
+    document.getElementById('inp').value = ''
+}
+
+
+function clr(){
+    document.getElementById('inp')
+    
+}
+
+
+
+
+
+
+
+
